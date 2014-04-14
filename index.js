47b63c863d41d86cc64ae1176f534a7dd1277d30
Alex <al.gaspar@gmail.com>
2014-04-14 17:50:10 +0200

    Read the dir and add files to the queue

--- index.js
+++ index.js
@@ -3,26 +3,39 @@ var readline = require('readline');
 var stream = require('stream');
 var moment = require('moment');
 var redis = require('redis');
+var async = require('async');
 
 var client = redis.createClient(6379, '127.0.0.1');
 
-var instream = fs.createReadStream('manycontacts-access.log');
-var outstream = new stream;
-var rl = readline.createInterface(instream, outstream);
-
+var PATH_TO_LOG = './logs/'
+var logs = fs.readdirSync(PATH_TO_LOG);
 var getRegex = /\[(.*)\].*\/bars\/get\/id\/(.{24})/;
 
-rl.on('line', function(line) {
-  var array = line.match(getRegex);
-  if(array) {
-    var date = moment(array[1].split(':')[0], "DD-MMM-YYYY");
-    var barId = array[2];
 
-    var key = barId + ':' + date.format('YYYYMMDD');
-    client.incr(key, redis.print);
-  }
+var queue = async.queue(function (file, callback) {
+  var instream = fs.createReadStream(PATH_TO_LOG + file);
+  var outstream = new stream();
+  var rl = readline.createInterface(instream, outstream);
+
+
+  rl.on('line', function(line) {
+    var array = line.match(getRegex);
+    if(array) {
+      var date = moment(array[1].split(':')[0], "DD-MMM-YYYY");
+      var barId = array[2];
+
+      var key = barId + ':' + date.format('YYYYMMDD');
+      client.incr(key, redis.print);
+    }
+  });
+
+  rl.on('close', function() {
+    callback();
+  });
+}, 1);
+
+
+logs.forEach(function(file) {
+  queue.push(file, function() { console.log(file + ' done.')});
 });
 
-rl.on('close', function() {
-  console.log('Done');
-});
